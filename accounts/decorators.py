cabe3b03973069ab92d9e9ef750f010b680d5b8b
DigoRM <rodrigormarcolino@gmail.com>
2023-04-03 08:19:01 -0300

    feedback_detail

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import user_passes_test
 def staff_required(view_func):
     decorated_view_func = user_passes_test(
         lambda u: u.is_active and u.is_staff,
-        login_url='/login/',
+        login_url='/accounts/login/',
         redirect_field_name=None
     )(view_func)
     return decorated_view_func
