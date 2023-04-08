811d891f4db25cf1a1c764537a9c16564a9716ab
DigoRM <rodrigormarcolino@gmail.com>
2023-04-07 23:31:50 -0300

    upload images

--- accounts/views.py
+++ accounts/views.py
@@ -228,33 +228,58 @@ def desmarcar_pago(request, pk):
 
     return redirect(redirect_url)
 
-
-# Perfil Aluno e Coach 
+from PIL import Image, ExifTags
 from io import BytesIO
-from PIL import Image
 from django.core.files.uploadedfile import InMemoryUploadedFile
-
-
-MAX_SIZE = 550
-
-
 
 def resize_image(image):
     if not image:
         return None
-    with Image.open(BytesIO(image.read())) as img:
-        max_size = max(img.width, img.height)
-        if max_size <= MAX_SIZE:
-            # Image is already small enough, no need to resize
-            return image
-        img.thumbnail((MAX_SIZE, MAX_SIZE))
-        image_io = BytesIO()
-        img.save(image_io, format='JPEG', quality=85)
-        image_file = InMemoryUploadedFile(image_io, None, image.name, 'image/jpeg',
-                                          image_io.getbuffer().nbytes, None)
-        return image_file
+
+    # Check if uploaded file is PNG
+    if image.name.endswith('.png'):
+        # Open PNG image and convert to RGB format
+        img = Image.open(image)
+        img = img.convert('RGB')
+    else:
+        img = Image.open(image)
+
+    # Get exif orientation
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+        exif = dict(img._getexif().items())
+        if exif[orientation] == 3:
+            img = img.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            img = img.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            img = img.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        # If there is no exif information, do nothing
+        pass
+
+    # Resize image
+    img = img.resize((550, 650), Image.ANTIALIAS)
+
+    # Save image to memory buffer as JPEG
+    buffer = BytesIO()
+    img.save(buffer, format='JPEG', quality=85)
+    buffer.seek(0)
+
+    # Determine content type of saved image
+    content_type = 'image/jpeg'
+    if image.name.endswith('.png'):
+        content_type = 'image/png'
+
+    return InMemoryUploadedFile(buffer, None, image.name, content_type,
+                                buffer.getbuffer().nbytes, None)
 
 
+
+
+# Perfil Aluno e Coach 
 @login_required(login_url='login')
 def edit_profile(request, pk=None):
     user = request.user
@@ -269,37 +294,33 @@ def edit_profile(request, pk=None):
         form2 = PerfilCoach(request.POST or None, request.FILES or None, instance=coach)
     else:
         raise Http404("This user is not an 'aluno' or a 'coach'.")
-    
-    # Image fields to be resized
+        
     image_fields = ['avatar', 'foto_frente', 'foto_lado', 'foto_verso']
-    
+
     if request.method == 'POST':
         if form and form.is_valid() or form2 and form2.is_valid():
             # Loop through image fields and resize uploaded images
             for field in image_fields:
                 if request.FILES.get(field):
                     image = request.FILES[field]
-                    img = Image.open(BytesIO(image.read()))
-                    img = resize_image(img)
-                    image_io = BytesIO()
-                    img.save(image_io, format='JPEG', quality=85)
-                    image_file = InMemoryUploadedFile(image_io, None, image.name, 'image/jpeg',
-                                                    image_io.getbuffer().nbytes, None)
+                    image_file = resize_image(image)
                     setattr(form.instance, field, image_file)
-                    
+            
+            # Save the form
             form.save() if form else form2.save()
+            
             messages.success(request, 'Perfil Atualizado!')
             return redirect('home')
         else:
-            messages.warning(request, form.errors if form else form2.errors)
+            messages.warning(request,form.errors if form else form2.errors)
             
     context = {
         'form': form,
         'form2': form2,
+
     }
     
     return render(request, 'alunos/edit_profile.html', context)
-
 
 @staff_required
 def perfil_aluno(request, pk):
@@ -345,7 +366,7 @@ def perfil_aluno(request, pk):
 
     return render(request, 'coach/perfil_aluno.html', context)
 
-#Feedback
+
 @login_required(login_url='login')
 def novo_feedbackV1(request, pk):
     user = request.user
@@ -359,15 +380,17 @@ def novo_feedbackV1(request, pk):
             feedback = form.save(commit=False)
             feedback.aluno = aluno
             feedback.coach = aluno.coach
-
-            # Resize the images before saving them
-            feedback.foto_frente = resize_image(feedback.foto_frente)
-            feedback.foto_lado = resize_image(feedback.foto_lado)
-            feedback.foto_verso = resize_image(feedback.foto_verso)
-
             feedback.protocolo = protocolo
-            feedback.save()
 
+            # Resize images
+            if feedback.foto_frente:
+                feedback.foto_frente = resize_image(feedback.foto_frente)
+            if feedback.foto_lado:
+                feedback.foto_lado = resize_image(feedback.foto_lado)
+            if feedback.foto_verso:
+                feedback.foto_verso = resize_image(feedback.foto_verso)
+
+            feedback.save()
             aluno.peso_atual = feedback.peso_atual
             aluno.save()
             messages.success(request, 'Feedback Enviado!')
@@ -383,7 +406,6 @@ def novo_feedbackV1(request, pk):
         'protocolo':protocolo,
     }
     return render(request, 'alunos/novo_feedback.html', context)
-
 
 
 @login_required
